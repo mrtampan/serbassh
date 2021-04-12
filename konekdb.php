5e2a0e5744ffbab860ab95f67fe4f87f5c9ea203
Achmad Rivaldi <mrtampan54@gmail.com>
2021-04-12 13:35:28 +0700

    first commit

---
+++ konekdb.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "Username@host: \n";
+
+echo "example: ucup@105.33.187.200 \n";
+
+$userhost = trim(fgets(STDIN));
+
+echo "localport:remotehost:port : \n";
+
+echo "example: 3310:localhost:3306 \n";
+
+$localhostport = trim(fgets(STDIN));
+
+if(empty($localhostport)){
+    echo "kosong auto handle";
+    $localhostport = "3310:localhost:3306";
+}
+
+echo shell_exec("ssh -f ". $userhost . " -L " . $localhostport . " -N");
+
+?>
